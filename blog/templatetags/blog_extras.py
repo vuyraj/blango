f7ecb9b5a63b67e55688440caad5901f2418caff
Yuvraj Sah <yuvraj7807@mbmcsit.edu.np>
2023-08-16 17:30:02 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,14 +2,14 @@ from django import template
 register = template.Library()
 
 from django.utils.html import format_html
-
+from blog.models import Post
 
 
 from django.contrib.auth import get_user_model
 user_model= get_user_model()
 
 @register.filter
-def author_details(author,current_use0r=None):
+def author_details(author,current_user=None):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
@@ -29,3 +29,34 @@ def author_details(author,current_use0r=None):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+# @register.simple_tag
+# def row():
+#     return format_html('<div class="row">')
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+# @register.simple_tag
+# def col():
+#     return format_html('<div class="col">')
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
